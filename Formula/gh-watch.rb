6eb09ddf0848a890183cb6e47cd23f07f8ea196b
Sam Rose <srose34@gmail.com>
2026-09-04 13:13:43 +0000

    fix: ship gh-watch as Homebrew bottles

--- Formula/gh-watch.rb
+++ Formula/gh-watch.rb
@@ -5,6 +5,13 @@ class GhWatch < Formula
   revision 1
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/masesor/homebrew-tap/releases/download/gh-watch-0.1.0"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a3386a1876119552bad5c1218f97080b3af98367aef5ad86e370c74f862ace59"
+    sha256 cellar: :any_skip_relocation, sequoia: "35f13eb4f9954496a0c57fa46bff16e9d438d10989d9c08d301a821fd21b182b"
+  end
+
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/masesor/gh-watch/releases/download/v#{version}/gh-watch_#{version}_darwin_arm64.tar.gz"
