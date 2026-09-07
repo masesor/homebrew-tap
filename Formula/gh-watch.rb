48bf178ad6a978171f738eb0f881d43703ceabce
Sam Rose <srose34@gmail.com>
2026-09-07 13:06:55 +0000

    chore: publish gh-watch 0.1.4 bottles

--- Formula/gh-watch.rb
+++ Formula/gh-watch.rb
@@ -4,6 +4,13 @@ class GhWatch < Formula
   version "0.1.4"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/masesor/homebrew-tap/releases/download/gh-watch-0.1.4"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "c5946879be87547bd7ddc1984787b9691bd4c072f401f7a6fc72fdd8409606c2"
+    sha256 cellar: :any_skip_relocation, sequoia: "3145435792c95c2c93ebada12be34dbccbc5a63a97ae3881015525c544bad0ac"
+  end
+
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/masesor/gh-watch/releases/download/v#{version}/gh-watch_#{version}_darwin_arm64.tar.gz"
