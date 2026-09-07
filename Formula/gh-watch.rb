92dfb4f7330637d8c9e5cd7959d9092150fe7f0c
Sam Rose <srose34@gmail.com>
2026-09-07 13:04:30 +0000

    chore: stage gh-watch 0.1.4 bottles

--- Formula/gh-watch.rb
+++ Formula/gh-watch.rb
@@ -1,23 +1,16 @@
 class GhWatch < Formula
   desc "Keyboard-first GitHub Actions and pull request TUI"
   homepage "https://github.com/masesor/gh-watch"
-  version "0.1.3"
+  version "0.1.4"
   license "MIT"
-
-  bottle do
-    root_url "https://github.com/masesor/homebrew-tap/releases/download/gh-watch-0.1.3"
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "2817893b1fbd346f7d5c4fc119a71bca2b38c382d088b2c984751493b4e40c81"
-    sha256 cellar: :any_skip_relocation, sequoia: "4cb402ab7eef72151b6ff468c9d0714971ee0622e4a385e2fbac8c0418adcf0b"
-  end
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/masesor/gh-watch/releases/download/v#{version}/gh-watch_#{version}_darwin_arm64.tar.gz"
-      sha256 "86ac9e6dd22ebb02d0d352eb3c4c46ca81bc543041ec87257028263aac867245"
+      sha256 "d9d80e4c2c2260e8939bc89a20b18a0fe6ca626fc5b5ab7e7cad7fbe0f83c56e"
     else
       url "https://github.com/masesor/gh-watch/releases/download/v#{version}/gh-watch_#{version}_darwin_amd64.tar.gz"
-      sha256 "fa89052d952d2c2ca2d93b44f667b80039cd40995c909447910e72ee1b5941b5"
+      sha256 "ee423992c9d7b378b05b98d6fd30e9f618ebfb6b52770d2767dcb78debeacd7e"
     end
   end
 
